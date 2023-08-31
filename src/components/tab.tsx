cc6a19e69b5746b1959fc5d8c8e3f2a0da594c7d
choimiae <chlaldo1220@gmail.com>
2023-08-31 09:15:32 +0900

    관리자 :: 변수명 변경

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -11,11 +11,11 @@ type TabCpType = {
 	data: TabType[]
 }
 
-function Tab({data} : TabCpType) {
+function Tab({data : tabData} : TabCpType) {
 	return(
 		<div className="mb-8">
 			{
-				data.map((item, index:number) => (
+				tabData.map((item, index:number) => (
 					<NavLink key={index} to={item.link} className={`mr-2 px-4 inline-flex items-center justify-center text-white h-10 ${item.activeFlag ? "bg-slate-500" : "bg-gray-300"}`}>{item.name}</NavLink>
 				))
 			}
