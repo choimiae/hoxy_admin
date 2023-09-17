1e4ea5f5a2b6e1ff213793be46b209a16b383da9
choimiae <chlaldo1220@gmail.com>
2023-09-17 17:54:40 +0900

    관리자 ::  firebase 연동 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import {BrowserRouter} from "react-router-dom";
 import App from './App';
 import {worker} from "./mocks/worker";
 
-if(process.env.NODE_ENV === "development") {
+//if(process.env.NODE_ENV === "development") {
     worker.start();
-}
+//}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
